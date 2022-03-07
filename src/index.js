4a164662ce392ca3be30f2ea42c14b38d3e04d2e
Geerun <bestgeerun@gmail.com>
2022-03-08 00:59:01 +0400

    add decode function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morzeString = '';
+    let result = '';
+    let tempString = '';
+
+    for (let i = 0; i <= ((expr.length)/10 - 1); i++) {
+        if (expr.slice(i*10,i*10+10) == "**********") {result += " "} 
+        else { 
+          tempString = expr.slice(i*10,i*10+10);
+          morzeString = '';
+          for (let j = 0; j <= 4; j++){
+            if (tempString.slice(j*2,j*2+2) == "10"){morzeString += '.'}
+            if (tempString.slice(j*2,j*2+2) == "11"){morzeString += '-'}
+          }
+          result += MORSE_TABLE[morzeString];
+              }
+    }
+    return result;
 }
+ 
 
 module.exports = {
     decode
 }
+
